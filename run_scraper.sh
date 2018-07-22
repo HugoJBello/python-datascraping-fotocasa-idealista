61be2a348239e5450871bf7fb9d8067442c31cdc
Hugo J. Bello <hugo.bello.gu@gmail.com>
2018-07-22 18:15:18 +0200

    fixed problems with script sh

--- run_scraper.sh
+++ run_scraper.sh
@@ -2,10 +2,10 @@
 PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 if [[ ! $(pgrep -f main.py) ]]; 
 	then
-		python3 main.py > bash.log;
-		nano main.py &;
-		sleep 30m;
-		killall nano;
+		python3 main.py > bash.log
+		nano main.py &
+		sleep 30m
+		killall nano
 	else
 		echo already running > bash.log
 fi
