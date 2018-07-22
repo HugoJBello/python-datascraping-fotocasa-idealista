8d3078f0fd543e93c91122fe8f2c3ba453588202
Hugo J. Bello <hugo.bello.gu@gmail.com>
2018-07-22 14:21:38 +0200

    fix

--- run_scraper.sh
+++ run_scraper.sh
@@ -2,7 +2,8 @@
 PATH=/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 if [[ ! $(pgrep -f main.py) ]]; 
 	then
-		python3 main.py > bash.log
+		python3 main.py > bash.log;
+		sudo killall python3
 	else
 		echo already running > bash.log
 fi
